336135f6b7e73411e8d31d4496a1efa2a803e415
ron <rinbar70@gmail.com>
2023-12-21 15:17:58 +0200

    Reorder code lines.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const items = [];
+    
     document.querySelector('form').addEventListener('submit', (function (ev) {
         ev.preventDefault();
-        console.log('onFormSubmit');
-        const tr = document.createElement('tr');
+
         /**
          * @type {HTMLInputElement}
          */
@@ -16,15 +16,20 @@ document.addEventListener('DOMContentLoaded', function () {
          * @type {HTMLInputElement}
          */
         const inputWho = document.getElementById('who');
+
         const item = {
             what: inputWhat.value,
             when: inputWhen.value,
             who: inputWho.value,
         };
-        items.push(item);
+        
         inputWhat.value = inputWhen.value = inputWho.value = '';
         inputWhat.focus();
+        
+        items.push(item);
         localStorage.setItem('todo', JSON.stringify(items));
+
+        const tr = document.createElement('tr');
         tr.innerHTML = `
             <td>${item.what}</td>
             <td>${item.when}</td>
